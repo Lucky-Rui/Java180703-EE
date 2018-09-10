18c434110d36524c8e216db32c026bdcf5753e83
Lucky-Rui <1269552287@qq.com>
2018-09-10 22:06:56 +0800

    MD5加密

--- src/com/situ/student/service/impl/UserServiceImpl.java
+++ src/com/situ/student/service/impl/UserServiceImpl.java
@@ -4,12 +4,15 @@ import com.situ.student.dao.IUserDao;
 import com.situ.student.dao.impl.UserDaoImpl;
 import com.situ.student.entity.User;
 import com.situ.student.service.IUserService;
+import com.situ.student.util.MD5Util;
 
 public class UserServiceImpl implements IUserService {
 	private IUserDao userDao = new UserDaoImpl();
 
 	@Override
 	public User login(String name, String password) {
+		// 将登陆的加密;登陆时加密的密码与注册时的密码对比.
+		// return userDao.findLogin(name, MD5Util.EncodeUtf8(password));
 		return userDao.findLogin(name, password);
 	}
 
@@ -21,6 +24,8 @@ public class UserServiceImpl implements IUserService {
 
 	@Override
 	public boolean register(User user) {
+		//注册时将密码加密
+		// user.setPassword(MD5Util.EncodeUtf8(user.getPassword()));
 		int count = userDao.insert(user);
 		return count == 1 ? true : false;
 	}
